cc17a1aa70d4f8008f6c27912121839225a175b6
rob-sim <rob-sim@users.noreply.github.com>
2025-11-20 11:52:43 +0000

    deploy: e88e28b3add38ec8bb61b7876b5375e62877ea85

---
+++ btn-hover-BBziuyNd.js
@@ -0,0 +1,2 @@
+import{a as e}from"./script-BqHq-8sC.js";function t(){if(e)return;document.querySelectorAll(".btn-hover").forEach(e=>{const t=e.querySelector(".btn");t&&(e.addEventListener("mouseenter",()=>{t.classList.add("hover")}),e.addEventListener("mouseleave",()=>{t.classList.remove("hover")}))})}export{t as default};
+//# sourceMappingURL=btn-hover-BBziuyNd.js.map
