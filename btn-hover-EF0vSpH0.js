faa7a1c5d4376171b8a444e33fed5e8e4c795e08
rob-sim <rob-sim@users.noreply.github.com>
2025-11-20 12:38:47 +0000

    deploy: bb06d48ba4868edcebffa6d85bdc8bc543a489b0

---
+++ btn-hover-EF0vSpH0.js
@@ -0,0 +1,2 @@
+import{a as e}from"./script-Coh7A2MN.js";function t(){if(e)return;document.querySelectorAll(".btn-hover").forEach(e=>{const t=e.querySelector(".btn");t&&(e.addEventListener("mouseenter",()=>{t.classList.add("hover")}),e.addEventListener("mouseleave",()=>{t.classList.remove("hover")}))})}export{t as default};
+//# sourceMappingURL=btn-hover-EF0vSpH0.js.map
